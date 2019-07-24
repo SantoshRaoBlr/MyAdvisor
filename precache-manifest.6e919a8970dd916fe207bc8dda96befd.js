d0953d7b2de3ecf91abb1f313207f48a5fb86707
Santosh Rao <santrao1@in.ibm.com>
2019-07-24 19:57:27 +0530

    Updates

---
+++ precache-manifest.6e919a8970dd916fe207bc8dda96befd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a02602c7c6080df376ff18e905dcb1",
+    "url": "/MyAdvisor/index.html"
+  },
+  {
+    "revision": "e1dbea6e65be3b28fe22",
+    "url": "/MyAdvisor/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/MyAdvisor/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "e1dbea6e65be3b28fe22",
+    "url": "/MyAdvisor/static/js/main.db265200.chunk.js"
+  },
+  {
+    "revision": "708142e4f988d027c5f8",
+    "url": "/MyAdvisor/static/js/runtime~main.83196cff.js"
+  }
+]);
